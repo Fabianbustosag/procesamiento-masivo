81a5020fa354475a222561afcec6d0221eb36c73
fabian <fabianbustosby@gmail.com>
2024-05-20 20:45:12 -0400

    funcion imprimir cada detalle

--- Tarea2/algo.py
+++
@@ -1,9 +0,0 @@
-from query_sql import query_POSTGRESQL 
-
-path = 'C:\\Users\\Fabian\\Documents\\Homelab\\Notebooks\\DB\\Tarea2'
-
-query = '''SELECT * FROM dtes;'''
-
-dtes = query_POSTGRESQL(path,'USM',query)
-
-
